f609bbcd2139b3f5c030203780cf8762a20f8c20
learn <andrey.ignor@gmail.com>
2023-02-03 23:14:27 +0300

    task0_done

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
 #include <iostream>
+#include <time.h>
+
+
+int main(){
+	int ar[1000] = {};
+	int size = 1000;
+	srand(time(nullptr));
+	for (int i = 0; i < size; i++){
+		ar[i] = rand() % 200;
+		std::cout << i << '\t' << ar[i] << '\n' << std::endl;
+	}
+	return 0;
+}
+
 
